4a9e1c05a7525497e4a97a9bc57af92602bf298c
Pepelapuerta <neozendra@gmail.com>
2025-03-04 18:45:40 +0100

    Update Router.jsx

--- web-page-2/src/Router/Router.jsx
+++ web-page-2/src/Router/Router.jsx
@@ -1,5 +1,16 @@
 import { EVENTS } from '../const';
 import { useState, useEffect } from "react";
+import PropTypes from 'prop-types'
+
+Router.propTypes = {
+  routes: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      Component: PropTypes.elementType.isRequired
+    })
+  ),
+  defaultComponent: PropTypes.elementType
+}
 export function Router ({routes = [], defaultComponent: DefaultComponent = () => <></>}) {
     const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
@@ -16,7 +27,15 @@ export function Router ({routes = [], defaultComponent: DefaultComponent = () =>
         }
     }, [])
 
-const Page = routes.find(({path}) => path === currentPath) ?.Component
-return Page ? <Page /> : <DefaultComponent />
+    const Page = routes.find(({path}) => path === currentPath)?.Component
+    if (!Page && !DefaultComponent) {
+      throw new Error('No matching route found and no default component provided')
+    }
+    
+return Page ? (
+    <Page />
+  ) : (
+    <DefaultComponent />
+  )
 
 }
